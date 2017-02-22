1cd1ee77a061d7ec0f3f159d4e2b763255de77b2
sashamartian <sasha.martian@yandex.ru>
2017-02-22 13:28:53 +0700

    Save

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task31.task3102;
+
+import java.io.IOException;
+import java.util.List;
+
+/* 
+Находим все файлы
+*/
+public class Solution {
+    public static List<String> getFileTree(String root) throws IOException {
+        return null;
+
+    }
+}
